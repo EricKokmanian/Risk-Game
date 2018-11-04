65316b285f8eb0e6d50def06053f33ba135c4b65
unknown <roger_lu_dude@hotmail.com>
2018-11-04 17:17:53 -0500

    part-6 fixed previous commit to include all the other files

---
+++ A2/part6/part6/Map.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <unordered_map>
+#include "Country.h"
+
+using namespace std;
+
+class Country;
+class Map {
+public:
+	Map();
+	void addCountry(string, Country*);
+	bool ifConnectedGraph(Country*);
+	bool ifConnectedContinent(Country*, Continent*);
+
+private:
+	unordered_map<string, Country*> worldMap;
+
+};
---
+++ A2/part6/part6/Player.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+#include "Player.h"
+#include <sstream>
+
+
+Player::Player()
+{
+	name = "";
+	numberOfArmy = 0;
+	dice = new Dice();
+}
+
+Player::Player(string playerName)
+{
+	name = playerName;
+	dice = new Dice();
+
+}
+
+Player::~Player() {
+	delete dice;
+}
+
+void Player::addDice(Dice* newDice)
+{
+	dice = newDice;
+}
+
+void Player::addCountry(Country* country)
+{
+	countries.push_back(country);
+}
+
+vector<Country*>& Player::getCountries() {
+	return countries;
+}
+
+void Player::removeCountry(Country* country) {
+	countries.erase(remove(countries.begin(), countries.end(), country), countries.end());
+}
+
+
+
+//string Player::getCountries()
+//{
+//	stringstream ss;
+//	for (size_t i = 0; i < countries.size(); ++i)
+//	{
+//		if (i != 0)
+//			ss << ",";
+//		ss << countries[i]->getCountryName();
+//	}
+//	string s = ss.str();
+//	return s;
+
+
+void Player::addContinent(Continent* continent)
+{
+	continents.push_back(continent);
+}
+
+void Player::setPlayerName(string playerName) {
+	name = playerName;
+}
+
+string Player::getPlayerName() {
+	return name;
+}
