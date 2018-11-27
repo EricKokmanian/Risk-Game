b272dc3bf21572acf0c6f4dc4a3d51cbd6945ff1
benadavid <benadavid_2007@hotmail.com>
2018-11-26 19:42:01 -0500

    cheater strategy

---
+++ A4/part 1/part 1/ConcreteStrategy4.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Strategy.h"
+#include "Player.h"
+#include "Map.h"
+
+using namespace std;
+
+// Class that will adopt the CHEATER PLAYER STRATEGY
+class ConcreteStrategy4 : public Strategy {
+public:
+	ConcreteStrategy4();
+	~ConcreteStrategy4();
+	void reinforce(Map, Player*);
+	void attack(Player*);
+	void fortify(Map, Player*);
+private:
+
+};
+
